7a4f3e5a389ccdb833fa810cb7c5bf31097ce2bd
Qasim Farooq <qasimfarooq631@gmail.com>
2024-10-04 09:57:07 +0500

    push

--- src/components/Seperaotor.jsx
+++ src/components/Seperaotor.jsx
@@ -1,18 +1,15 @@
-import { StyleSheet, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, View} from 'react-native';
+import React from 'react';
 
 const Seperaotor = () => {
-  return (
-    <View style={styles.container}>
-    </View>
-  )
-}
+  return <View style={styles.container}></View>;
+};
 
 const styles = StyleSheet.create({
-    container: {
-        height: 2,
-        backgroundColor: '#CAD5E2'
-    }
-})
+  container: {
+    height: 2,
+    backgroundColor: '#CAD5E2',
+  },
+});
 
-export default Seperaotor
+export default Seperaotor;
--- src/screens/Details.jsx
+++ src/screens/Details.jsx
@@ -1,15 +1,12 @@
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import {StyleSheet, Text, View, ScrollView, Image} from 'react-native';
 import React from 'react';
 
-export default function Details({ route }) {
-  const { contact } = route.params;
+export default function Details({route}) {
+  const {contact} = route.params;
   return (
     <ScrollView style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image 
-          style={styles.image}
-          source={{ uri: contact.imageUrl }} 
-        />
+        <Image style={styles.image} source={{uri: contact.imageUrl}} />
       </View>
       <View style={styles.detailsContainer}>
         <Text style={styles.name}>{contact.name}</Text>
@@ -32,24 +29,21 @@ const styles = StyleSheet.create({
   image: {
     width: 150,
     height: 150,
-    borderRadius: 75,   // Rounded image
+    borderRadius: 75,
     borderColor: '#dcdcdc',
     borderWidth: 2,
   },
   detailsContainer: {
     marginTop: 20,
     paddingHorizontal: 20,
-    // alignItems: 'center',
   },
   name: {
-    // textAlign: 'center',
     fontWeight: 'bold',
     color: '#333',
     fontSize: 24,
     marginTop: 10,
   },
   phone: {
-    // textAlign: 'center',
     color: '#666',
     fontSize: 18,
     marginTop: 5,
